21fe479fc887232f2fe6c036ea46a81c0ed4a5d9
nyangoto <nyangotoorangi@gmail.com>
2023-11-21 06:53:44 +0300

    Updated data ingestion & cleaning logic

--- app.py
+++ app.py
@@ -6,35 +6,31 @@ from scipy.sparse import csr_matrix
 import xgboost as xgb
 
 def clean_data(data):
-    # Assuming your data needs cleaning (replace this with actual cleaning logic)
-    cleaned_data = data.dropna()  # Placeholder for actual cleaning process
+    cleaned_data = data.dropna()
     return cleaned_data
 
 def analyze_data(data):
-    # Analyze user preferences, average ratings, etc.
+    
     user_avg_ratings = data.groupby('userId')['rating'].mean()
     movie_avg_ratings = data.groupby('movieId')['rating'].mean()
         
     return user_avg_ratings, movie_avg_ratings
 
 def recommend_movies(user_id, user_avg_ratings, movie_avg_ratings, train_sparse_matrix):
-    # Your movie recommendation algorithm goes here
-    # Implement robust measures to handle irregular or incomplete data
-    
-    # Example: Fallback to popular movies if user data is insufficient
+    # movie recommendation algorithm
+   
     if user_id not in user_avg_ratings:
         popular_movies = movie_avg_ratings.nlargest(5).index
         return movie_avg_ratings.loc[popular_movies]
     
-    # Your personalized recommendation logic based on user ratings
-    # Example: Consider movies that the user has rated highly
+    # personalized recommendation logic
+    
     user_high_ratings = train_sparse_matrix[user_id, :].toarray().ravel() > user_avg_ratings[user_id]
-    recommended_movies = movie_avg_ratings[user_high_ratings].nlargest(5)
+    recommended_movies = movie_avg_ratings[user_high_ratings].nlargest(3)
     
     return recommended_movies
 
 def create_cli_interface():
-    # Design a simple Command Line Interface (CLI)
     while True:
         user_name = input("Enter your name (or 'exit' to quit): ")
         
@@ -51,7 +47,7 @@ def create_cli_interface():
 
 if __name__ == "__main__":
     # Load and clean data
-    raw_data = pd.read_csv('user_movie_ratings.csv')
+    raw_data = pd.read_csv('data.txt', header=None, names=['User', 'Movie', 'Rating'])
     cleaned_data = clean_data(raw_data)
 
     # Analyze data
@@ -60,7 +56,7 @@ if __name__ == "__main__":
     # Create a sparse matrix
     train_sparse_matrix = csr_matrix((cleaned_data.rating.values, (cleaned_data.userId.values, cleaned_data.movieId.values)))
 
-    # Train your recommendation model (if applicable)
+    # Model training and shaping
 
     # User ID mapping for CLI
     user_name_to_id_mapping = dict(zip(cleaned_data['userName'], cleaned_data['userId']))
